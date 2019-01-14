4de037b4d60b018ed24bbf2adbbe3e84d85289ca
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-14 00:44:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
 def reverse_each_word(phrase)
   phrase_array = phrase.split(" ")
-  #puts phrase_array.inspect
+  reversed_array = word_reversed_array(phrase_array)
+  reversed_phrase = reversed_array.join(" ")
+end
+
+def word_reversed_array(array)
   phrase_array.collect do |word|
     word.reverse
   end
-  phrase_array.join(" ")
 end
